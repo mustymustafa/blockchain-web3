92673689084c0efbcf2e0c4fda06bdc2058958f8
musty <mustafaumar70@live.com>
2022-03-04 23:47:02 +0000

    solidity

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -3,5 +3,38 @@ pragma solidity ^0.8.0;
 
 contract Transactions {
     //type unassigned integer 256 bits in size
-   uint256 transactionCounter; 
+   uint256 transactionCount; 
+
+
+     event Transfer(address from, address receiver, uint amount, string message, uinit256 timestamp, string keyword);
+
+     //create a structure
+
+     struct TransferStruct {
+     address sender;
+     address receiver;
+     uint amount;
+     string message;
+     uint256 timestamp;
+     string keyword;   
+     }
+
+    // let transactions = an array of type TransferStruct
+     TransferStruct[] transactions;
+
+     function addToBlockChain(address payable receiver, uint amount, string memory message, string memory keyword) public {
+         transactionCount += 1;
+         transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
+         //make the transfeer
+         emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
+     }
+
+     function getAllTransactions() public view returns (TransferStruct[] memory) {
+                return transactions;
+     }
+
+     function getTransactionCount() public view returns (uint256) {
+            return transactionCount;
+     }
+
 }
---
+++ smart_contract/scripts/deploy.js
@@ -0,0 +1,29 @@
+import hre from 'hardhat'
+
+
+const main = async () => {
+ 
+
+  // We get the contract to deploy
+  const Transactions = await hre.ethers.getContractFactory("Transactions");
+  const transactions = await Greeter.deploy();
+
+  await transactions.deployed();
+
+  console.log("transactions deployed to:", transactions.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch {
+console.error(error);
+process.exit(1);
+  }
+}
+
+runMain();
